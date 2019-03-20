20bea4f542d071de4014b6f1f57025ecc8974356
Manvitha <meetmanvitha@gmail.com>
2019-03-20 19:13:48 +0530

    Update ExportManifestController.java

--- ExportManifestController.java
+++ ExportManifestController.java
@@ -1,9 +1,13 @@
 package com.ge.capital.dms.fr.sle.controllers.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +16,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.box.sdk.BoxAPIConnection;
+import com.box.sdk.BoxFile;
+import com.box.sdk.BoxFolder;
+import com.box.sdk.BoxItem;
+import com.ge.capital.dms.dao.DocumentServiceDAO;
+import com.ge.capital.dms.entity.UserDetails;
 import com.ge.capital.dms.model.ExportManifestIVO;
 import com.ge.capital.dms.model.FileMetadata;
+import com.ge.capital.dms.service.DocumentService;
+import com.ge.capital.dms.service.UpdateService;
+import com.ge.capital.dms.utility.AkanaToken;
+import com.ge.capital.dms.utility.DmsUtilityConstants;
+import com.ge.capital.dms.utility.DmsUtilityService;
+import com.google.common.collect.Iterators;
 
 import  java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Proxy.Type;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import  org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -33,6 +53,7 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.util.IOUtils;
+import org.json.simple.JSONObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.poi.hssf.usermodel.HSSFCell;
@@ -47,16 +68,44 @@ import  org.apache.poi.hssf.usermodel.HSSFRow;
 @RestController
 @RequestMapping("/secure")
 public class ExportManifestController {
+	@Value("${upload.path}")
+	private String UPLD_DIR;
+
+	@Value("${upload.max.file.count}")
+	private String MAX_FILE_CNT;
+
+	@Value("${upload.rootFolderId}")
+	private String UPLD_RT_FLDR_ID;
+
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	UpdateService updateService;
+
+	@Autowired
+	DmsUtilityService dmsUtilityService;
+
+	@Autowired
+	DocumentServiceDAO documentServiceDAO;
+	
+	@Autowired
+	DocumentService documentService;
 	
 	@Value("${download.path}")
 	private String DIRECTORY;
 	
 	@SuppressWarnings("deprecation")
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
 	@RequestMapping(value = "/exportManifest")
 	@ResponseBody
-	public ResponseEntity<HSSFWorkbook> exportToExcel(@RequestBody ExportManifestIVO request) {
-		System.out.println(request);
-		try {
+	public ResponseEntity<Resource> exportToExcel(HttpServletRequest httprequest, @RequestBody ExportManifestIVO request) {
+		String loggedinUser=httprequest.getHeader("loggedinuser");
+		
+			String boxId = "";
+			/* UserDetails userDetails = (UserDetails) session.getAttribute("UserDetails");
+			System.out.println(session.getAttributeNames());
+			System.out.println(session.getAttribute("UserDetails")); */
 			String filename = null;
 			String sendersName = request.getSenderName();
 			String storerNumber = request.getStorerNum();
@@ -65,6 +114,10 @@ public class ExportManifestController {
 			String trackingNumber = request.getTrackingNum();
 			String sendersBusiness = request.getSenderBusiness();
 			String shippingMethod = request.getShippingMethod();
+			//String creation_to_dt_searched= null;
+			//String creation_frm_dt_searched= null;
+			//String mft_modifier_searched= null;
+			//String mft_creater_searched= null;
 			ArrayList<String> obj1 = (ArrayList<String>) request.getManifestInfo();
 			//Map[] map = new HashMap[];
 			
@@ -104,6 +157,7 @@ public class ExportManifestController {
             	
             	String[] tem = s.split(",");
             	for(int i = 0;i< tem.length;i++) {
+            		System.out.println(tem[i]);
             		if(tem[i].split(":")[1].replace('"',' ').replace('}',' ').replace('{',' ').trim() != null || tem[i].split(":")[1].replace('"',' ').replace('}',' ').replace('{',' ').trim() != "null") {
             			rown.createCell(i).setCellValue(tem[i].split(":")[1].replace('"',' ').replace('}',' ').replace('{',' ').trim());
             		} else {
@@ -116,22 +170,88 @@ public class ExportManifestController {
             
 			filename = "Manifest_Reports";
 			String filename1 = DIRECTORY + "\\" + filename + "_" + manifestSeqNumber + ".xls";
+			Properties docIdprops = dmsUtilityService.loadPropertiesFile(DmsUtilityConstants.docIdMappingResource);
+            AkanaToken akanaToken = dmsUtilityService.generateAkanaAccessToken();
+			JSONObject boxTokenJSON = dmsUtilityService.generateBoxAccessToken(akanaToken.getAccess_token());
+			String boxToken = boxTokenJSON.get("accessToken").toString();
+			try {
             FileOutputStream fileOut = new FileOutputStream(filename1);
-            //File f = new File(filename1);
+            File f = new File(filename1);
             workbook.write(fileOut);
             System.out.println("Your excel file has been generated!");
-            HashMap<String, String> metdata = new HashMap<String, String>();
+            
+            
+            
+			if (!boxToken.isEmpty()) {
+				BoxAPIConnection api = new BoxAPIConnection(boxToken);
+				java.net.Proxy proxy = new java.net.Proxy(Type.HTTP,
+						new InetSocketAddress("PITC-Zscaler-Americas-Cincinnati3PR.proxy.corporate.ge.com", 80));
+				api.setProxy(proxy);
+				BoxFolder rootFolder = new BoxFolder(api, UPLD_RT_FLDR_ID);
+				String folderPath = null;
+				folderPath = env.getProperty("upload.folderPath.exportManifest");
+				String path[] = folderPath.split("\\\\");
+				
+				String fid = rootFolder.getID();
+				for (String p : path) {
+					BoxFolder folder = new BoxFolder(api, fid);
+					for (BoxItem.Info itemInfo : folder) {
+						if (itemInfo instanceof BoxFolder.Info && itemInfo.getName().equals(p)) {
+							fid = itemInfo.getID();
+							break;
+						}
+					}
+				}
+				String actualFolderId = fid;
+				BoxFolder fldr = new BoxFolder(api, actualFolderId);
+				FileInputStream stream = new FileInputStream(filename1);
+				BoxFile.Info newFileInfo = fldr.uploadFile(stream,
+						filename1);
+				boxId = newFileInfo.getID();
+				System.out.println(newFileInfo.getName() + ":uploaded to box successfully...");
+				HashMap<String, String> metdata = new HashMap<String, String>();
+				metdata.put("manifestSeqNumber", manifestSeqNumber);
+				metdata.put("businessLocation", businessLocation);
+				metdata.put("storerNumber", storerNumber);
+				metdata.put("sendersName", sendersName);
+				metdata.put("docID", newFileInfo.getID());
+				metdata.put("docName", filename + "_" + manifestSeqNumber + ".xls");
+				System.out.println(newFileInfo.getID());
+				/*metdata.put("mft_creater_searched", mft_creater_searched);
+				metdata.put("mft_modifier_searched", mft_modifier_searched);
+				metdata.put("creation_frm_dt_searched", creation_frm_dt_searched);
+				metdata.put("creation_to_dt_searched", creation_to_dt_searched); */
+				metdata.put("mft_creater_searched", loggedinUser);
+				metdata.put("mft_modifier_searched", "");
+				metdata.put("creation_frm_dt_searched", "");
+				metdata.put("creation_to_dt_searched", "");
+				updateService.updateManifestDoc("exportManifest",metdata);
+				documentService.updateAuditInfo(newFileInfo.getID(), "ExportManifest", "Manifest", loggedinUser, "SUCCESS",
+						"Exported successfully");
+			}
+			InputStreamResource resource = null;
+			resource = new InputStreamResource(new FileInputStream(filename1));
             return ResponseEntity.ok()
     				// Content-Disposition
-    				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;").header("fileName", filename)
-    				.body(workbook);
+    				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;").header("fileName", filename+"_"+manifestSeqNumber+".xls")
+    				.body(resource);
            // fileOut.close();
             //workbook.close();
             
 		} catch(Exception e) {
-			e.printStackTrace();
-			return null;
+			//e.printStackTrace();
+			BoxAPIConnection api = new BoxAPIConnection(boxToken);
+			BoxFile boxFile = new BoxFile(api, boxId);
+			documentService.updateAuditInfo(boxId, "ExportManifest", "Manifest", loggedinUser, "FAILED",
+					"Export failed");
+			boxFile.delete();
+			documentServiceDAO.deleteDoc(boxId, "exportManifest");
+			return ResponseEntity.badRequest()
+    				// Content-Disposition
+    				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;")
+    				.body(new InputStreamResource(null, "File Not Found"));
 		}
 		   
     }
 }
+
